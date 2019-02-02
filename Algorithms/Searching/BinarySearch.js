c67ee4866fbb71311aa5e1c815b5dc3bfd5d5431
Calvin Wong <calwong42991@gmail.com>
2019-02-02 15:39:07 -0800

    Binary Search completed

---
+++ Algorithms/Searching/BinarySearch.js
@@ -0,0 +1,26 @@
+const Comparator = require('../utils/comparator/comparator');
+
+const binarySearch = (sortedArray, seekElement, comparatorCallback) => {
+  const comparator = new Comparator(comparatorCallback);
+
+  let startIndex = 0;
+  let endIndex = sortedArray.length - 1;
+
+  while (startIndex <= endIndex) {
+    const middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+    if (comparator.equal(sortedArray[middleIndex], seekElement)) {
+      return middleIndex;
+    }
+
+    if (comparator.lessThan(sortedArray[middleIndex], seekElement)) {
+      startIndex = middleIndex + 1;
+    } else {
+      endIndex = middleIndex - 1;
+    }
+  }
+
+  return -1;
+}
+
+module.exports = binarySearch;
