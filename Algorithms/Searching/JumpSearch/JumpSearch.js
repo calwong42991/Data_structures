b59dd97ad6113d51b811478eeb9964df8e344f66
Calvin Wong <calwong42991@gmail.com>
2019-02-03 14:54:08 -0800

    Jump Search completed

---
+++ Algorithms/Searching/JumpSearch/JumpSearch.js
@@ -0,0 +1,36 @@
+const Comparator = require('../../utils/comparator/comparator');
+
+const JumpSearch = (sortedArray, seekElement, comparatorCallback) => {
+  const comparator = new Comparator(comparatorCallback);
+  const arraySize = sortedArray.length;
+
+  if (!arraySize) {
+    return -1;
+  }
+
+  const jumpSize = Math.floor(Math.sqrt(arraySize));
+
+  let blockStart = 0;
+  let blockEnd = jumpSize;
+  while (comparator.greaterThan(seekElement, sortedArray[Math.min(blockEnd, arraySize) - 1])) {
+    blockStart = blockEnd;
+    blockEnd += jumpSize;
+
+    if (blockStart > arraySize) {
+      return -1;
+    }
+  }
+
+  let currentIndex = blockStart;
+  while (currentIndex < Math.min(blockEnd, arraySize)) {
+    if (comparator.equal(sortedArray[currentIndex], seekElement)) {
+      return currentIndex;
+    }
+
+    currentIndex += 1;
+  }
+
+  return -1;
+}
+
+module.exports = JumpSearch;
