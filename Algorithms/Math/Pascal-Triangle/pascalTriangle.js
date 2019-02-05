084d9b685929064e63624721775faf0e3a9275d3
Calvin Wong <calwong42991@gmail.com>
2019-02-05 12:57:35 -0800

    Pascal Triangle complete

---
+++ Algorithms/Math/Pascal-Triangle/pascalTriangle.js
@@ -0,0 +1,13 @@
+const pascalTriangle = (lineNumber) => {
+  const currentLine = [1];
+
+  const currentLineSize = lineNumber + 1;
+
+  for (let numIndex = 1; numIndex < currentLineSize; numIndex += 1) {
+    currentLine[numIndex] = currentLine[numIndex - 1] * (lineNumber - numIndex + 1) / numIndex;
+  }
+
+  return currentLine;
+}
+
+module.exports = pascalTriangle;
