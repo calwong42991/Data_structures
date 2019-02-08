1592d8df5b37c706a2f5be878c319661ce0f4d56
Calvin Wong <calwong42991@gmail.com>
2019-02-08 13:18:11 -0800

    Two Sum Hash completed

--- Toy_Problems/LeetCode/01_Two.Sum.js
+++ Toy_Problems/LeetCode/01_Two.Sum.js
@@ -1,6 +1,6 @@
 /* https://leetcode.com/problems/two-sum/ */
 
-const twoSum = function(nums, target){
+const twoSum = (nums, target) => {
   for(let i = 0; i < nums.length; i++){
     for(let j = i + 1; j < nums.length; j++){
       if(nums[j] == target - nums[i]){
@@ -11,3 +11,24 @@ const twoSum = function(nums, target){
   }
   return false;
 }
+
+const twoSumHash = (nums, target) => {
+  let len = nums.length;
+  if(len < 2) return nums;
+  let hashTable = {};
+
+  for(let i = 0; i < len; i++){
+    if(hashTable[target - nums[i]]){
+      return [hashTable[target - nums[i]], i];
+    } else {
+      hashTable[nums[i]] = i;
+    }
+  }
+  return false;
+}
+
+let arr = [1,2,3,4,5,6,];
+let target = 9;
+
+console.log(twoSumHash(arr, target));
+
