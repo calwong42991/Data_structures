160024495537bc9c522c7dec86ae3324056ee228
Calvin Wong <calwong42991@gmail.com>
2019-02-08 13:44:51 -0800

    Reverse String for loop solution

--- Toy_Problems/LeetCode/344_ReverseString.js
+++ Toy_Problems/LeetCode/344_ReverseString.js
@@ -1,1 +1,11 @@
 /* https://leetcode.com/problems/reverse-string/ */
+
+const reverseString = (s) => {
+  let result = "";
+  for(let i = s.length - 1; i >= 0; i--){
+    result += s[i];
+  }
+  return result;
+}
+
+console.log(reverseString('something'));
