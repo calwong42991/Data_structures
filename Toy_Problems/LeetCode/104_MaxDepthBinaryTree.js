10ca016c8f4e3597477495e03f86616e36ce7fb8
Calvin Wong <calwong42991@gmail.com>
2019-02-09 14:07:00 -0800

    fixed maxDepth

--- Toy_Problems/LeetCode/104_MaxDepthBinaryTree.js
+++ Toy_Problems/LeetCode/104_MaxDepthBinaryTree.js
@@ -4,6 +4,6 @@ const maxDepth = (root) => {
   if(root === null){
     return 0;
   }
-  return 1 + Math.max(maxDepth(root.left). maxDepth(root.right));
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 }
 
