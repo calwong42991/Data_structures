cb712e5ace8b49ca9f2f58ebe6e1116b9139ac84
Calvin Wong <calwong42991@gmail.com>
2019-02-09 13:55:10 -0800

    Max Depth attempt

--- Toy_Problems/LeetCode/104_MaxDepthBinaryTree.js
+++ Toy_Problems/LeetCode/104_MaxDepthBinaryTree.js
@@ -1,1 +1,33 @@
 /* https://leetcode.com/problems/maximum-depth-of-binary-tree/ */
+
+const maxDepth = (root) => {
+  let maxD = 0;
+  const recurse = (current, depth) => {
+    if (current.val === null) {
+      return;
+    }
+
+    if (maxD <= depth) {
+      maxD = depth;
+    }
+
+    if (current.left) {
+      if(maxD <= depth){
+        recurse(current.left, depth);
+      } else {
+        recurse(current.left, depth += 1);
+      }
+    }
+
+    if (current.right) {
+      if(maxD <= depth){
+        recurse(current.right, depth);
+      } else {
+        recurse(current.right, depth += 1);
+      }
+    }
+  }
+
+  recurse(root, 1);
+  return maxD;
+}
