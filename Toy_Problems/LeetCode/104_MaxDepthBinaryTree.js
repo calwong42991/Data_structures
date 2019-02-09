1ba4225f8d4dc5575a0e4a73c30f28eb1ecfbb5c
Calvin Wong <calwong42991@gmail.com>
2019-02-09 13:57:48 -0800

    Max Depth Binary Tree completed

--- Toy_Problems/LeetCode/104_MaxDepthBinaryTree.js
+++ Toy_Problems/LeetCode/104_MaxDepthBinaryTree.js
@@ -1,33 +1,9 @@
 /* https://leetcode.com/problems/maximum-depth-of-binary-tree/ */
 
 const maxDepth = (root) => {
-  let maxD = 0;
-  const recurse = (current, depth) => {
-    if (current.val === null) {
-      return;
-    }
-
-    if (maxD <= depth) {
-      maxD = depth;
-    }
-
-    if (current.left) {
-      if(maxD <= depth){
-        recurse(current.left, depth);
-      } else {
-        recurse(current.left, depth += 1);
-      }
-    }
-
-    if (current.right) {
-      if(maxD <= depth){
-        recurse(current.right, depth);
-      } else {
-        recurse(current.right, depth += 1);
-      }
-    }
+  if(root === null){
+    return 0;
   }
-
-  recurse(root, 1);
-  return maxD;
+  return 1 + Math.max(maxDepth(root.left). maxDepth(root.right));
 }
+
