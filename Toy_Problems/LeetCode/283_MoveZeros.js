8f46817b5bd07839990f2983a501095aeea8702d
Calvin Wong <calwong42991@gmail.com>
2019-02-10 13:48:07 -0800

    Move zeros completed

--- Toy_Problems/LeetCode/283_MoveZeros.js
+++ Toy_Problems/LeetCode/283_MoveZeros.js
@@ -1,1 +1,20 @@
 /* https://leetcode.com/problems/move-zeroes/ */
+
+const moveZeroes = (nums) => {
+  let pointer = 0;
+  let end = nums.length;
+
+  while(pointer < end){
+    if(nums[pointer] === 0){
+      nums.push(0);
+      nums.splice(pointer, 1)
+    }
+    pointer += 1;
+  }
+
+  return nums;
+}
+
+let arr = [0, 1, 0, 3, 12];
+
+console.log(moveZeroes(arr));
