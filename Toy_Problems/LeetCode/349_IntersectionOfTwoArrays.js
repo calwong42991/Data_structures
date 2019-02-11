3194b0ab5b2925db7f5a8cba6971f93b6cf7b01d
Calvin Wong <calwong42991@gmail.com>
2019-02-11 10:32:03 -0800

    359 intersection of two arrays completed

--- Toy_Problems/LeetCode/349_IntersectionOfTwoArrays.js
+++ Toy_Problems/LeetCode/349_IntersectionOfTwoArrays.js
@@ -1,1 +1,29 @@
 /* https://leetcode.com/problems/intersection-of-two-arrays/ */
+
+const intersection = (num1, num2) => {
+  let hash = {};
+  let num = [];
+  
+  if (num1.length === 0 || num2.length === 0) {
+    return num;
+  }
+
+  for (let i = 0; i < num1.length - 1; i++) {
+    if (!hash[num1[i]]) {
+      hash[num1[i]] = true;
+    }
+  }
+
+  for (let j = 0; j < num2.length - 1; j++) {
+    if (hash[num2[j]] && num.indexOf(num2[j]) < 0) {
+      num.push(num2[j]);
+    }
+  }
+
+  return num;
+}
+
+let arr1 = [4, 9, 5];
+let arr2 = [9, 4, 9, 8, 4];
+
+console.log(intersection(arr1, arr2));
