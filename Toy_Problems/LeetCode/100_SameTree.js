10e292b50117dc90c6ae80caf9db16c1057d8a21
Calvin Wong <calwong42991@gmail.com>
2019-02-11 11:28:31 -0800

    Same Tree completed

--- Toy_Problems/LeetCode/100_SameTree.js
+++ Toy_Problems/LeetCode/100_SameTree.js
@@ -1,1 +1,13 @@
 /* https://leetcode.com/problems/same-tree/ */
+
+const isSameTree = (tree1, tree2) => {
+  if(!tree1 && !tree2){
+    return true;
+  }
+
+  if(!tree1 || !tree2 || tree1.val !== tree2.val){
+    return false;
+  }
+
+  return isSameTree(tree1.left, tree2.left) && isSameTree(tree1.right, tree2.right)
+}
