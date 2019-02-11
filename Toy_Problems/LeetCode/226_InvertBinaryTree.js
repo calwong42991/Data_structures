a6f465884df80bf3b11cef2b3ea45989e79dfcad
Calvin Wong <calwong42991@gmail.com>
2019-02-10 19:00:37 -0800

    Invert Binary Tree Recurssive function

--- Toy_Problems/LeetCode/226_InvertBinaryTree.js
+++ Toy_Problems/LeetCode/226_InvertBinaryTree.js
@@ -1,1 +1,15 @@
 /* https://leetcode.com/problems/invert-binary-tree/ */
+
+const invertTree = (root) => {
+  if (!root) {
+    return root
+  };
+
+  let tmp = root.left;
+  root.left = root.right;
+  root.right = tmp;
+
+  invertTree(root.left);
+  invertTree(root.right);
+  return root;
+}
