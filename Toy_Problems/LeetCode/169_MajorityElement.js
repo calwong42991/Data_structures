0319ed2c23d06e8039b90b538219b8e89ea8d60f
Calvin Wong <calwong42991@gmail.com>
2019-02-13 11:14:15 -0800

    169 Majority Element own solution

--- Toy_Problems/LeetCode/169_MajorityElement.js
+++ Toy_Problems/LeetCode/169_MajorityElement.js
@@ -1,1 +1,18 @@
-/* https://leetcode.com/problems/majority-element/ */ 
+/* https://leetcode.com/problems/majority-element/ */
+
+const majorityElement = (nums) => {
+  let hash = {};
+  let output;
+  nums.forEach(element => {
+    hash[element] = hash[element] ? hash[element] + 1 : hash[element] = 1;
+  });
+
+  Object.keys(hash).forEach((e) => {
+    if (hash[e] > nums.length/2) {
+      output = e;
+    }
+  })
+  return output;
+}
+
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
