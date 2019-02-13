0557d822f7633e471a8cff8011d1c6f3b93409a8
Calvin Wong <calwong42991@gmail.com>
2019-02-12 16:29:30 -0800

    Valid anagram refactored to using 1 hash

--- Toy_Problems/LeetCode/242_validAnagram.js
+++ Toy_Problems/LeetCode/242_validAnagram.js
@@ -1,31 +1,21 @@
 /* https://leetcode.com/problems/valid-anagram/ */
 
 const isAnagram = (s, t) => {
-  if(s.length !== t.length){
+  if (s.length !== t.length) {
     return false;
   }
 
-  let hashS = {};
-  let hashT = {};
+  let hash = {};
 
   s.split('').map(e => {
-    hashS[e] ? hashS[e] += 1 : hashS[e] = 1;
+    hash[e] = hash[e] ? hash[e] + 1 : 1;
   })
 
   t.split('').map(e => {
-    hashT[e] ? hashT[e] += 1 : hashT[e] = 1;
+    hash[e] = hash[e] ? hash[e] - 1 : -1;
   })
 
-  console.log(hashS);
-  console.log(hashT);
-  
-  let keys = Object.keys(hashS);
-  for(let i = 0; i < keys.length; i++){
-    if(hashS[keys[i]] !== hashT[keys[i]]){
-      return false;
-    }
-  }
-  return true;
+  return Object.keys(hash).every(k => hash[k] === 0);
 }
 
 console.log(isAnagram('anagram', 'nagaram'));
