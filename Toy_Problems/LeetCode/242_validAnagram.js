9a8a3c9a6f0b6725329fc90b85ee58db5ac65444
Calvin Wong <calwong42991@gmail.com>
2019-02-12 16:06:49 -0800

    valid anagram solution simple approach

--- Toy_Problems/LeetCode/242_validAnagram.js
+++ Toy_Problems/LeetCode/242_validAnagram.js
@@ -1,1 +1,12 @@
 /* https://leetcode.com/problems/valid-anagram/ */
+
+const isAnagram = (s, t) => {
+  if(s.length !== t.length){
+    return false;
+  }
+
+  let reverseS = s.split('').reverse().join('');
+  return reverseS === t;
+}
+
+console.log(isAnagram('anagram', 'margana'));
