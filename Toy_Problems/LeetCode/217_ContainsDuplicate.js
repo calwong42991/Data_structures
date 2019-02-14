2cf567a976909ad619411c197beac073cb486170
Calvin Wong <calwong42991@gmail.com>
2019-02-13 22:45:47 -0800

    faster solution

--- Toy_Problems/LeetCode/217_ContainsDuplicate.js
+++ Toy_Problems/LeetCode/217_ContainsDuplicate.js
@@ -1,15 +1,30 @@
 /* https://leetcode.com/problems/contains-duplicate/ */
 
 const containsDuplicate = (nums) => {
+  // let hash = {};
+
+  // nums.forEach(element => {
+  //   hash[element] = hash[element] ? hash[element] + 1 : 1;
+  // });
+
+  // for(let key in hash){
+  //   if(hash[key] > 1){
+  //     return true;
+  //   }
+  // }
+  // return false;
+
+
   let hash = {};
 
-  nums.forEach(element => {
-    hash[element] = hash[element] ? hash[element] + 1 : 1;
-  });
-
-  return Object.values(hash).some((element) => {
-    return element > 1;
-  });
+  for(let elm of nums){
+    if(hash[elm]){
+      return hash[elm];
+    }else {
+      hash[elm] = true;
+    }
+  }
+  return false
 }
 
-console.log(containsDuplicate([1,2,3]));
+console.log(containsDuplicate([1,2,3, 1]));
