5a41a7ed07b9f4d2a7bc7fbef3b5d4b8fa57b675
Calvin Wong <calwong42991@gmail.com>
2019-02-13 22:36:02 -0800

    217 contains duplicates first attempt

--- Toy_Problems/LeetCode/217_ContainsDuplicate.js
+++ Toy_Problems/LeetCode/217_ContainsDuplicate.js
@@ -1,5 +1,15 @@
 /* https://leetcode.com/problems/contains-duplicate/ */
 
 const containsDuplicate = (nums) => {
+  let hash = {};
 
+  nums.forEach(element => {
+    hash[element] = hash[element] ? hash[element] + 1 : 1;
+  });
+
+  return Object.values(hash).some((element) => {
+    return element > 1;
+  });
 }
+
+console.log(containsDuplicate([1,2,3]));
