a751a580af51e7f68fdc9d6c69ccb5e152ef1c79
Calvin Wong <calwong42991@gmail.com>
2019-02-14 11:47:31 -0800

    intersectionSorted completed

--- Toy_Problems/LeetCode/350_IntersectionofTwoArraysII.js
+++ Toy_Problems/LeetCode/350_IntersectionofTwoArraysII.js
@@ -17,8 +17,33 @@ const intersect = (nums1, nums2) => {
   }
 
   return output;
-
 }
 
-console.log(intersect([1, 2, 2, 1], [2, 2]));
-console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]));
+const intersectSorted = (nums1, nums2) => {
+  nums1.sort((a,b) => a - b)
+  nums2.sort((a,b) => a - b)
+
+  let pointer1 = 0;
+  let pointer2 = 0;
+
+  let result = [];
+
+  while(pointer1 < nums1.length && pointer2 < nums2.length){
+    if(nums1[pointer1] === nums2[pointer2]){
+      result.push(nums1[pointer1]);
+      pointer1++;
+      pointer2++;
+    } else if(nums1[pointer1] > nums2[pointer2]){
+      pointer2++;
+    } else {
+      pointer1++;
+    }
+  }
+
+  return result;
+}
+
+// console.log(intersect([1, 2, 2, 1], [2, 2]));
+// console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]));
+console.log(intersectSorted([1, 2, 2, 1], [2, 2]));
+console.log(intersectSorted([4, 9, 5], [9, 4, 9, 8, 4]));
