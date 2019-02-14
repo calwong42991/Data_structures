3b9d6e3dfa3ca8bb386e7c4a504c2f3f8caf8480
Calvin Wong <calwong42991@gmail.com>
2019-02-14 11:38:43 -0800

    350 completed

--- Toy_Problems/LeetCode/350_IntersectionofTwoArraysII.js
+++ Toy_Problems/LeetCode/350_IntersectionofTwoArraysII.js
@@ -1,5 +1,24 @@
 /* https://leetcode.com/problems/intersection-of-two-arrays-ii/ */
 
 const intersect = (nums1, nums2) => {
-  
+
+  let hash = {};
+  let output = [];
+
+  for (let elm1 of nums1) {
+    hash[elm1] = hash[elm1] ? hash[elm1] + 1 : 1;
+  }
+
+  for (let elm2 of nums2) {
+    if (hash[elm2]) {
+      output.push(elm2);
+      hash[elm2]--;
+    }
+  }
+
+  return output;
+
 }
+
+console.log(intersect([1, 2, 2, 1], [2, 2]));
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]));
