5ba99d0eec2d15f649f471544aa01700bed10554
Calvin Wong <calwong42991@gmail.com>
2019-02-15 11:07:08 -0800

    convert to title alt solution

--- Toy_Problems/LeetCode/168_excelSheetColumnTitle.js
+++ Toy_Problems/LeetCode/168_excelSheetColumnTitle.js
@@ -1,15 +1,26 @@
 /* https://leetcode.com/problems/excel-sheet-column-title/ */
 
-let dict = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let dict = ['0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 const convertToTitle = (n) => {
-  if(n < 27){
+  if (n < 27) {
     return dict[n];
   } else {
-    let temp = Math.ceil(n/26) - 1;
+    let temp = Math.ceil(n / 26) - 1;
     return convertToTitle(temp) + convertToTitle(n - temp * 26);
   }
-  
+}
+
+const convertToTitleAlt = (n) => {
+  let res = "";
+  let codeA = "A".charCodeAt();
+  while( n > 0){
+    n--;
+    res = String.fromCharCode(codeA + n % 26) + res;
+    n = parseInt(n / 26);
+  }
+  return res;
 }
 
 console.log(convertToTitle(100))
+console.log(convertToTitleAlt(100))
