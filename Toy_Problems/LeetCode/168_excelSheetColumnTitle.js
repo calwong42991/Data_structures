57492dd14835d86e2017dbb17672432d6bdb3fad
Calvin Wong <calwong42991@gmail.com>
2019-02-15 10:52:29 -0800

    168 excel sheet column title recursive solution

--- Toy_Problems/LeetCode/168_excelSheetColumnTitle.js
+++ Toy_Problems/LeetCode/168_excelSheetColumnTitle.js
@@ -1,1 +1,15 @@
 /* https://leetcode.com/problems/excel-sheet-column-title/ */
+
+let dict = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+const convertToTitle = (n) => {
+  if(n < 27){
+    return dict[n];
+  } else {
+    let temp = Math.ceil(n/26) - 1;
+    return convertToTitle(temp) + convertToTitle(n - temp * 26);
+  }
+  
+}
+
+console.log(convertToTitle(100))
