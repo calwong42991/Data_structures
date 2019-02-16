7bfba8d16f6a346ee47378a333720a273b32bb8a
Calvin Wong <calwong42991@gmail.com>
2019-02-16 11:58:10 -0800

    reverse linked list completed

--- 206_ReverseLinkedList.js
+++ 206_ReverseLinkedList.js
@@ -1,1 +1,12 @@
 /* https://leetcode.com/problems/reverse-linked-list/ */
+
+const reverseList = (head) => {
+  let prev = null;
+  while(head){
+    var next = head.next;
+    head.next = prev;
+    prev = head;
+    head = next;
+  }
+  return prev;
+}
