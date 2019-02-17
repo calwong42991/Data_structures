3e80c4d4031520c1ce641464134f02c1fa8d5190
Calvin Wong <calwong42991@gmail.com>
2019-02-17 13:51:12 -0800

    first uniq char completed

--- Toy_Problems/LeetCode/387_FirstUniqueChar.js
+++ Toy_Problems/LeetCode/387_FirstUniqueChar.js
@@ -1,1 +1,20 @@
 /* https://leetcode.com/problems/first-unique-character-in-a-string/ */
+
+const firstUniqChar = (s) => {
+  var seen = {};
+  for (i = 0; i < s.length; i++) {
+    if (seen[s.charAt(i)] === undefined) {
+      seen[s.charAt(i)] = 1;
+    } else {
+      seen[s.charAt(i)] += 1;
+    }
+  }
+  var unique = -1;
+  for (i = 0; i < s.length; i++) {
+    if (seen[s.charAt(i)] === 1) {
+      unique = i;
+      break;
+    }
+  }
+  return unique;
+}
