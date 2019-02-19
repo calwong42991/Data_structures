54dc6f10f19b2a41dc1f034a2c55de96bb9b7209
Calvin Wong <calwong42991@gmail.com>
2019-02-18 17:08:09 -0800

    first uniq char with indexof and lastindexof

--- Toy_Problems/LeetCode/387_FirstUniqueChar.js
+++ Toy_Problems/LeetCode/387_FirstUniqueChar.js
@@ -18,3 +18,12 @@ const firstUniqChar = (s) => {
   }
   return unique;
 }
+
+const firstUniqCharIndex = (s) => {
+  for(let i = 0; i < s.length; i++){
+    if(s.indexOf(s[i]) === s.lastIndexOf(s[i])){
+      return i;
+    }
+  }
+  return -1;
+}
