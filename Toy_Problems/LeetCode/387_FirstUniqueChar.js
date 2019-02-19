73e178aa2f7a832c6856c912545d8f655974b4cd
Calvin Wong <calwong42991@gmail.com>
2019-02-18 17:09:49 -0800

    first uniq char with map

--- Toy_Problems/LeetCode/387_FirstUniqueChar.js
+++ Toy_Problems/LeetCode/387_FirstUniqueChar.js
@@ -20,10 +20,28 @@ const firstUniqChar = (s) => {
 }
 
 const firstUniqCharIndex = (s) => {
-  for(let i = 0; i < s.length; i++){
-    if(s.indexOf(s[i]) === s.lastIndexOf(s[i])){
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
       return i;
     }
   }
   return -1;
 }
+
+var firstUniqCharMap = (s) => {
+  var map = new Map();
+  for (i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      map.set(s[i], 2);
+    } else {
+      map.set(s[i], 1);
+    }
+  }
+
+  for (i = 0; i < s.length; i++) {
+    if (map.has(s[i]) && map.get(s[i]) === 1) {
+      return i;
+    }
+  }
+  return -1;
+};
