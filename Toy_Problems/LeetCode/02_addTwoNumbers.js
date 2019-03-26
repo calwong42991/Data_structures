1dd343c5ba1facc9f103d302b490962170a33e86
Calvin Wong <calwong42991@gmail.com>
2019-03-25 21:37:17 -0700

    add two numbers completed

--- Toy_Problems/LeetCode/02_addTwoNumbers.js
+++ Toy_Problems/LeetCode/02_addTwoNumbers.js
@@ -1,5 +1,28 @@
 /* https://leetcode.com/problems/add-two-numbers/description/ */
 
 var addTwoNumbers = function (l1, l2) {
-  
+
+  var c = 0;
+  var ret = new ListNode(0);
+  var curr = ret;
+
+  while (l1 || l2) {
+      var a = l1 ? l1.val : 0;
+      var b = l2 ? l2.val : 0;
+      var sum = a + b + c;
+      c = Math.floor(sum / 10);
+      curr.next = new ListNode(sum % 10);
+      if (l1) {
+          l1 = l1.next;
+      }
+      if (l2) {
+          l2 = l2.next;
+      }
+      curr = curr.next;
+  }
+  if (c) {
+      curr.next = new ListNode(c);
+  }
+
+  return ret.next;
 };
