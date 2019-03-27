75e1a8b1c519f37359895489969a1e788121cec6
Calvin Wong <calwong42991@gmail.com>
2019-03-26 20:50:40 -0700

    longest substring without repeating complete

--- Toy_Problems/LeetCode/03_longestSubStringWithoutRepeating.js
+++ Toy_Problems/LeetCode/03_longestSubStringWithoutRepeating.js
@@ -2,4 +2,29 @@
 
 const lengthOfLongestSubstring = (s) => {
 
+  let max = 0;
+  let i = 0;
+  let j = 0;
+  let n = s.length;
+  let map = {};
+
+  while (i < n && j < n) {
+    if (map[s[j]] === undefined) {
+      map[s[j]] = 1;
+      j++;
+      max = Math.max(max, j - i);
+    } else {
+      delete map[s[i]];
+      i++;
+    }
+  }
+  return max;
 }
+
+
+console.log(lengthOfLongestSubstring('c'), 1);
+console.log(lengthOfLongestSubstring(''), 0);
+console.log(lengthOfLongestSubstring('abcabcbb'), 3);
+console.log(lengthOfLongestSubstring('bbbbb'), 1);
+console.log(lengthOfLongestSubstring('pwwkew'), 3);
+console.log(lengthOfLongestSubstring('xhhyccrcbdczkvzeeubynglxfdedshtpobqsdhufkzgwuhaabdzrlkosnuxibrxssnkxuhcggkecshdvkcmymdqbxolbfjtzyfw'), 14);
