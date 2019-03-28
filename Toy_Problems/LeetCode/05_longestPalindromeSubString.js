7e6e6e01b9ec4f7b1aa1e0606d522fb493144b7a
Calvin Wong <calwong42991@gmail.com>
2019-03-27 19:44:26 -0700

    longest palindrom substring complete

--- Toy_Problems/LeetCode/05_longestPalindromeSubString.js
+++ Toy_Problems/LeetCode/05_longestPalindromeSubString.js
@@ -1,5 +1,29 @@
 /* https://leetcode.com/problems/longest-palindromic-substring/description/ */
 
-const longtestPalindrome = (s) => {
+const longestPalindrome = (s) => {
+  var n = s.length;
+  var res = '';
+  var dp = [];
+
+  while (dp.push(new Array(n).fill(-1)) < n);
+
+  for (var i = n - 1; i >= 0; i--) {
+    for (var j = i; j < n; j++) {
+      dp[i][j] = s[i] === s[j] && ((j - i < 3) || dp[i + 1][j - 1]);
+      if (dp[i][j] === undefined) {
+        console.log(i, j, s[i], s[j], dp[i + 1][j - 1])
+      }
+      if (dp[i][j]) {
+        var tmp = s.substring(i, j + 1);
+        if (tmp.length > res.length) res = tmp;
+      }
+
+    }
+  }
+
+  return res;
 
 }
+
+
+console.log(longestPalindrome('babad'));
